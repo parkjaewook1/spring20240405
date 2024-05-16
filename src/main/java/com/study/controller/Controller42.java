a886310e4079f9874862670b6ca10a111d39ab83
jaewook <wodnr5050@gmail.com>
2024-05-16 15:39:30 +0900

    Map 객체 응답

--- src/main/java/com/study/controller/Controller42.java
+++ src/main/java/com/study/controller/Controller42.java
@@ -1,5 +1,7 @@
 package com.study.controller;
 
+import com.study.domain.MyBean411;
+import com.study.domain.MyBean412;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -46,11 +48,36 @@ public class Controller42 {
     @GetMapping("sub4")
     @ResponseBody
     public Map<String, Object> method4() {
-        return Map.of("name", "강인",
-                "age", 26,
-                "married", false,
-                "info", Map.of("team", "PSG",
-                        "location", "paris"),
-                "item", List.of("축구화", "보호대", "물"));
+        return Map.of("city", "서울",
+                "height", 188.88,
+                "foods", List.of("pizza", "coffee"),
+                "desc", Map.of("team", "london", "location", "uk"),
+                "hasAccess", false);
+    }
+
+    @GetMapping("sub5")
+    @ResponseBody
+    public MyBean411 method5() {
+        MyBean411 obj = new MyBean411();
+        obj.setName("이강인");
+        obj.setAge(55);
+
+        return obj; // json으로 직렬화
+    }
+
+    @GetMapping("sub6")
+    @ResponseBody
+    public MyBean412 method6() {
+        MyBean412 myBean412 = new MyBean412();
+        myBean412.setFoods(List.of("감자", "고구마"));
+        myBean412.setHeight(177.7);
+        myBean412.setMarried(false);
+        MyBean411 myBean411 = new MyBean411();
+        myBean411.setName("흥민");
+        myBean411.setAge(55);
+        myBean412.setInfo(myBean411);
+        myBean412.setName("강인");
+        return myBean412;
+
     }
 }
